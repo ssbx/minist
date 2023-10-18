e6fff1aa3be153ded18756c04f436029f7d6af79
seb <seb@seb.seb>
2023-10-18 11:38:18 +0200

    m

--- mem.c
+++ mem.c
@@ -1,1 +1,9 @@
+#include <stdlib.h>
 #include "mem.h"
+
+struct Memory* mem_new() {
+    struct Memory* m = malloc(sizeof(struct Memory));
+    m->objc = 0;
+    m->objs = NULL;
+    return m;
+}
--- mem.h
+++ mem.h
@@ -1,4 +1,10 @@
 #ifndef MEM_H
 #define MEM_H
 
+struct Memory {
+    int   objc;
+    char *objs;
+};
+
+struct Memory* mem_new();
 #endif // MEM_H
